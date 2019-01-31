816bfa1bd39bfd8205b8b46f1c672bad1a1e3000
User <User@DESKTOP-VBU9GG9>
2019-01-31 21:20:22 +0900

    이미지 업로드 추가

--- routes/moment.js
+++ routes/moment.js
@@ -1,45 +1,61 @@
-const multer = require('multer');
+var multer = require('multer');
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+//var upload = multer({ dest: 'uploads/', limits: { fileSize: 5 * 1024 * 1024 } });
 
-const upload = multer({ dest: 'uploads/', limits: { fileSize: 5 * 1024 * 1024 } });
-
-const path = require('path');
-//const upload = multer({
-//  storage: multer.diskStorage({
-//    destination: function (req, file, cb) {
-//      cb(null, 'uploads/');
-//    },
-//    filename: function (req, file, cb) {
-//      cb(null, new Date().valueOf() + path.extname(file.originalname));
-//    }
-//  }),
-//});
+var path = require('path');
+var upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, new Date().valueOf() + path.extname(file.originalname));
+    }
+  }),
+});
 
 var pool 		= require('./mysqlConn');
 
-//router.post('/imgup', upload.single('test4'), (req, res) => {
+//router.post('/imgup', upload.single('imginput'), (req, res) => {
 //	  console.log(req.file); 
 //});
 
-//app.post('/upload', upload.single('userfile'), function(req, res){
-//	  res.send('Uploaded! : '+req.file); // object를 리턴함
-//	  console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
-//	});
 
-router.post('/imgup', function(req, res, next) {
-	console.log(req.body);
-    console.log(req.files);
-	  upload(req, res).then(function (file) {
-	    res.json(file);
-	  }, function (err) {
-	    res.send(500, err);
-	  });
+router.post('/imgup', upload.single('file'), function(req, res){
+	  res.send(req.file.path); // object를 리턴함
+	  console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
 	});
+
+
+//router.post('/imgup', function(req, res, next) {
+////    console.log(req);
+//    console.log("req.body");
+//    console.log(req.file);
+//
+//});
+
+//router.post('/imgup', function(req, res, next) {
+//	console.log(req);
+//	console.log("req.body");
+//	console.log(req.body);
+//    console.log(req.files);
+//	  upload(req, res).then(function (file) {
+//	    res.json(file);
+//	  }, function (err) {
+//	    res.send(500, err);
+//	  });
+//	});
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/uploads', function(req, res, next) {
+	console.log("asdasdasdasdasdadasds")
+	  res.send('respond with a resource');
+	});
 
 router.get('/header', function(req,res,next){
 	res.render('./moment/header');
@@ -66,6 +82,12 @@ router.get('/view_detail2', function(req, res, next) {
 	res.render('./moment/view_detail2',{data:dnum} );
 //	res.send('aaaaa');
 });
+router.get('/header', function(req,res,next){
+	res.render('./moment/header');
+	});
+router.get('/footer', function(req,res,next){
+	res.render('./moment/footer');
+	});
 
 router.get('/mem_login', function(req,res,next){
 	res.render('./moment/mem_login');
@@ -97,6 +119,46 @@ router.get('/list', function(req,res,next){
   }); 
 });
 
+router.get('/listinit', function(req,res,next){
+	var d_kind =req.param('kind');
+	console.log(d_kind)
+	  pool.getConnection(function (err, connection) {
+	     if (d_kind == 0) {
+	    	 var sql = "SELECT * FROM data_tbl as data LEFT OUTER JOIN member_tbl as mem ON data.m_no = mem.m_no order by d_regdate desc;";
+	    	 connection.query(sql, function (err, rows) {
+	    	  console.log(sql)
+	    		 if (err) console.error("err : " + err);
+	    		 
+	    		 res.send(rows);
+	    		 connection.release();
+	    	 });
+		}else{
+		  var sql = "SELECT * FROM data_tbl as data LEFT OUTER JOIN member_tbl as mem ON data.m_no = mem.m_no where data.d_kind = ? order by d_regdate desc;";
+		  connection.query(sql,[d_kind], function (err, rows) {
+			  console.log(sql)
+	    		 if (err) console.error("err : " + err);
+	    		 
+	    		 res.send(rows);
+	    		 connection.release();
+			  
+		  });
+		}
+	  });
+	});
+
+router.get('/home', function(req,res,next){
+	  pool.getConnection(function (err, connection) {
+	      var sql = "SELECT * FROM data_tbl as data LEFT OUTER JOIN member_tbl as mem ON data.m_no = mem.m_no order by d_regdate desc;";
+
+	      connection.query(sql, function (err, rows) {
+//	    	  console.log(rows)
+	          if (err) console.error("err : " + err);
+	    	  
+	          res.render('./moment/home', {data: rows});
+	          connection.release();
+	      });
+	  }); 
+	});
 router.get('/home_mypage', function(req,res,next){
 	var dnum =req.param('num');
 	console.log("dnuddddm")
@@ -110,20 +172,6 @@ router.get('/upload', function(req,res,next){
 	console.log(dnum)
 	res.render('./moment/upload',{data:dnum} );
 });
-
-router.get('/listinit', function(req,res,next){
-	  pool.getConnection(function (err, connection) {
-	      var sql = "SELECT * FROM data_tbl as data LEFT OUTER JOIN member_tbl as mem ON data.m_no = mem.m_no order by d_regdate desc;";
-
-	      connection.query(sql, function (err, rows) {
-//	    	  console.log(rows)
-	          if (err) console.error("err : " + err);
-	    	  
-	          res.send(rows);
-	          connection.release();
-	      });
-	  });
-	});
 
 router.get('/box_select', function(req,res,next){
 	  pool.getConnection(function (err, connection) {
@@ -140,11 +188,11 @@ router.get('/box_select', function(req,res,next){
 	});
 
 router.get('/mylike_selectdb', function(req,res,next){
+	var m_email =req.param('m_email');
 	  pool.getConnection(function (err, connection) {
-	      var sql = "SELECT * FROM (member_tbl as mem LEFT OUTER JOIN like_tbl as mylike ON mem.m_no = mylike.m_no) LEFT OUTER JOIN data_tbl as mydata ON mylike.d_no = mydata.d_no LEFT OUTER JOIN member_tbl as mymem ON mydata.m_no = mymem.m_no;";
-
-	      connection.query(sql, function (err, rows) {
-//	    	  console.log(rows)
+	      var sql = "SELECT * FROM (member_tbl as mem LEFT OUTER JOIN like_tbl as mylike ON mem.m_no = mylike.m_no) LEFT OUTER JOIN data_tbl as mydata ON mylike.d_no = mydata.d_no LEFT OUTER JOIN member_tbl as mymem ON mydata.m_no = mymem.m_no where mymem.m_email = ?";
+	      connection.query(sql,[m_email], function (err, rows) {
+	    	  console.log(sql)
 	          if (err) console.error("err : " + err);
 	    	  
 	    	  res.send(rows);
@@ -154,10 +202,10 @@ router.get('/mylike_selectdb', function(req,res,next){
 	});
 
 router.get('/myrecord_selectdb', function(req,res,next){
+	var m_email =req.param('m_email');
 	  pool.getConnection(function (err, connection) {
-	      var sql = "SELECT * FROM member_tbl as mem LEFT OUTER JOIN data_tbl as mydata ON mem.m_no = mydata.m_no;";
-
-	      connection.query(sql, function (err, rows) {
+	      var sql = "SELECT * FROM member_tbl as mem LEFT OUTER JOIN data_tbl as mydata ON mem.m_no = mydata.m_no where mem.m_email=? ";
+	      connection.query(sql,[m_email], function (err, rows) {
 //	    	  console.log(rows)
 	          if (err) console.error("err : " + err);
 	    	  
@@ -197,18 +245,19 @@ router.get('/mem_selectdb', function(req,res,next){
 
 router.get('/uploaddb', function(req,res,next){
 	   var m_no 		= req.param("m_no");
-	   var d_kind     	= req.param("d_kind");
+	   var d_kind     	= parseInt(req.param("d_kind"));
 	   var d_location   = req.param("d_location");
+//	   var d_regdate    = new Date();
 	   var d_title   	= req.param("d_title");
 	   var d_content   	= req.param("d_content");
-	   var d_star 		= req.param("d_star");
+	   var d_star 		= parseInt(req.param("d_star"));
 	   var d_path 		= req.param("d_path");
-	   var count   = "";
+	   var d_like   = "";
 	   
 	  pool.getConnection(function (err, connection) {
 	      var sql = "insert into data_tbl(m_no,d_regdate,d_kind,d_location,d_title,d_content,d_path,d_star,d_like)values(?,sysDate(),?,?,?,?,?,?,0)";
 	      
-	      connection.query(sql,[m_no,d_kind,d_location,d_title,d_content,d_star,d_path], function (err, rows) {
+	      connection.query(sql,[m_no,d_kind,d_location,d_title,d_content,d_path,d_star], function (err, rows) {
 //	    	  console.log(rows)
 	          if (err) console.error("err : " + err);
 	    	  
@@ -329,7 +378,7 @@ router.post('/likecnt', function(req,res,next){
 	console.log("라이크씨앤티")
 	  pool.getConnection(function (err, connection) {
 	      var sql = "UPDATE data_tbl SET d_like=?  WHERE d_no=?";
-	      connection.query(sql,[d_no,d_like], function (err, rows) {
+	      connection.query(sql,[d_like,d_no], function (err, rows) {
 	    	  console.log(rows)
 	          if (err) console.error("err : " + err);
 	    	  
@@ -375,9 +424,9 @@ router.get('/likeup', function(req,res,next){
 router.get('/likedown', function(req,res,next){
 	var d_no = req.param("dnum");
 	var m_no = req.param("mnum");
-	console.log("라이크업")
+	console.log("라이크다운")
 	  pool.getConnection(function (err, connection) {
-	      var sql = "delete from  like_tbl where d_no=? and m_no=?";
+	      var sql = "delete from like_tbl where d_no=? and m_no=?";
 	      connection.query(sql,[d_no,m_no], function (err, rows) {
 	    	  console.log(rows)
 	          if (err) console.error("err : " + err);
